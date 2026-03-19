c8bc796f7a6566fcf897344814598e2edef2ec56
piotr-yuxuan <piotr-yuxuan@users.noreply.github.com>
2026-03-19 04:24:39 +0100

    Add font configuration and custom font derivations
    fonts.nix: shared font list imported by Home Manager and nix-darwin.
    packages/tw-moe-fonts.nix: Taiwan Ministry of Education standard fonts.
    packages/zapfino.nix: Zapfino calligraphic font derivation.

---
+++ packages/tw-moe-fonts.nix
@@ -0,0 +1,28 @@
+{ stdenvNoCC, fetchFromGitHub }:
+
+stdenvNoCC.mkDerivation {
+  pname = "tw-moe-fonts";
+  version = "2020-11-14";
+
+  src = fetchFromGitHub {
+    owner = "Jiehong";
+    repo = "TW-fonts";
+    rev = "2020-11-14";
+    sha256 = "1b8wgbbky9vz5jmqphk8c53l4xbd0gnpcdcqk87j4mkpxz2bq37n";
+  };
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm644 *.ttf -t $out/share/fonts/truetype/tw-moe
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Taiwan Ministry of Education standard Kai and Sung fonts (教育部標準楷書/宋體)";
+    homepage = "https://language.moe.gov.tw/";
+    license = { # Open Government Data License v1.0 (≈ CC-BY 4.0)
+      free = true;
+      url = "https://data.gov.tw/license";
+    };
+  };
+}
---
+++ packages/zapfino.nix
@@ -0,0 +1,28 @@
+{ stdenvNoCC, fetchurl, unzip }:
+
+stdenvNoCC.mkDerivation {
+  pname = "zapfino";
+  version = "1";
+
+  src = fetchurl {
+    url = "https://font.download/dl/font/zapfino.zip";
+    sha256 = "0dwbf21rmzvyzzpa110ipm8ain03vrawsngbg2qs0pnndn1mwsh6";
+  };
+
+  nativeBuildInputs = [ unzip ];
+
+  unpackPhase = ''
+    unzip $src
+  '';
+
+  installPhase = ''
+    runHook preInstall
+    install -Dm644 *.ttf -t $out/share/fonts/truetype/zapfino
+    runHook postInstall
+  '';
+
+  meta = {
+    description = "Zapfino calligraphic typeface by Hermann Zapf";
+    homepage = "https://font.download/font/zapfino";
+  };
+}
